1f588442ae7e327a4c2229c149f28f4206445bdf
Matthew Olsen <mbolsen@gmail.com>
2024-09-03 11:35:55 -0600

    Added TODO feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import type { Schema } from "@/amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { StorageImage, StorageManager } from "@aws-amplify/ui-react-storage";
 import { Card, Flex, Text, Button } from "@aws-amplify/ui-react";
-import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
+import React from "react";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
-
 Amplify.configure(outputs);
-
 const client = generateClient<Schema>();
 
 export default function App() {
@@ -24,28 +22,27 @@ export default function App() {
   }
 
   function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+    client.models.Todo.delete({ id });
   }
-  
-  function createTodo({ key, content }: { key: string;  content: string }) {
+
+  useEffect(() => {
+    listTodos();
+  }, []);
+
+  function createTodo({ key, content }: { key: string; content: string }) {
     client.models.Todo.create({
       content,
       key,
     });
   }
-      
-  useEffect(() => {
-    listTodos();
-  }, []);
 
   return (
     <main>
       <h1>My todos</h1>
-      {/* <button onClick={createTodo}>+ new</button> */}
       <ul>
         {todos.map((todo) => (
           <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
-            <Flex justifyContent="space-between">
+            <Flex justifyContent={"space-between"}>
               <Text>{todo.content}</Text>
               {todo.key ? (
                 <StorageImage
@@ -53,40 +50,33 @@ export default function App() {
                   alt={todo.content || ""}
                   width="100px"
                 />
-              ) : null }
+              ) : null}
             </Flex>
           </li>
         ))}
       </ul>
       <StorageManager
-        path={"medio/"}
+        path="media/"
         acceptedFileTypes={["image/*"]}
         maxFileCount={1}
         onUploadStart={({ key }) => {
-          const content = window.prompt("Todos content");
+          const content = window.prompt("Todo content");
           if (!key || !content) return;
-          createTodo({key, content})
+          createTodo({ key, content });
         }}
         components={{
           Container({ children }) {
-            return <Card variation="elevated">{children}</Card>
+            return <Card variation="elevated">{children}</Card>;
           },
           FilePicker({ onClick }) {
             return (
               <Button variation="primary" onClick={onClick}>
                 Add Todo and Choose File For Upload
               </Button>
-            )
-          }
+            );
+          },
         }}
       />
-      {/* <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div> */}
     </main>
   );
 }
